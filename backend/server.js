3f25327bbf279ac88d8a9fc89eb3bd8b93a1c7f0
vykq <103254648+vkidik@users.noreply.github.com>
2024-06-25 14:15:18 +1100

    Update server.js without MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,112 +1,131 @@
 const tools = require("./toolsAPI");
 const express = require('express');
-const mongoose = require('mongoose');
-const http = require('http');
 const cors = require('cors');
-const { log } = require("console");
+const fs = require('fs');
+const path = require('path');
+const { Buffer } = require('buffer');
 
-let ip = '', host = ''
-
-require('dotenv').config();
-
-const PORT = process.env.SERVER_PORT || 3000;
-const DB_NAME = process.env.DB_NAME;
-const COLLECTION_NAME = process.env.COLLECTION_NAME
-
-mongoose.connect(`mongodb+srv://vladgro720:zkCWVEd65RB3HETO@cluster0.1o5p1nv.mongodb.net/${DB_NAME}`);
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'Ошибка подключения к MongoDB:'));
-db.once('open', () => {
-  console.log('Успешное подключение к MongoDB');
-});
-
-const Schema = mongoose.Schema;
-const dataSchema = new Schema({
-  tableId: String,
-  sheetName: String
-}, { versionKey: false });
-
-const DataModel = mongoose.model('Data', dataSchema, COLLECTION_NAME);
+const host = 'https://ya.ecit.ru';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, './public')));
 
 app.post('/api/data', async (req, res) => {
     try {
-      const { tableURL, sheetName } = req.body;
-      const tableId = tableURL.split('/spreadsheets/d/')[1].split('/')[0];
-      let table = await DataModel.findOne({ tableId, sheetName }) 
+        const { tableURL, sheetName } = req.body;
+        const tableId = tableURL.split('/spreadsheets/d/')[1].split('/')[0];
+        
+        const data = await tools.getDataFromGoogleSheet(tableId, sheetName);
 
-      if(table) throw new Error('Таблица с таким листом уже существует')
-      
-      const data = await tools.getDataFromGoogleSheet(tableId, sheetName);
+        if (!data.state) {
+            if (data.message === 'Ошибка при получение данных с таблицы: The caller does not have permission') {
+                throw new Error('Ошибка при получение данных с таблицы: \nПроблемы с доступом таблицы. Проверьте, что она действительно является публичной или ввели правильную ссылку!');
+            }
+            throw new Error(data.message);
+        }
 
-      if (!data.state){
-        if(data.message == 'Ошибка при получение данных с таблицы: The caller does not have permission') throw new Error('Ошибка при получение данных с таблицы: \nПроблемы с доступом таблицы. Проверьте, что она действительно является публичной или ввели правильную ссылку!')
-        throw new Error(data.message)
-      }
+        console.log(`${host}/api/data/${tableId}__${Buffer.from(sheetName).toString('base64')}/append`);
 
-      table = await new DataModel({ tableId, sheetName }).save() 
-
-      res.send({ 
-        state: true, 
-        data: {
-          rows: data.data.rows,
-          url: host+'/api/data/'+table._id+'/append'
-        } 
-      });
+        res.send({
+            state: true,
+            data: {
+                rows: data.data.rows,
+                url: `${host}/api/data/${tableId}__${Buffer.from(sheetName).toString('base64')}/append`
+            }
+        });
     } catch (error) {
-      console.error(error);
-      res.status(200).json({ state: false, message: error.message });
+        console.error(error);
+        res.status(200).json({ state: false, message: error.message });
     }
 });
 
 app.get('/api/data/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const table = await DataModel.findById(id);
+    try {
+        const ids = req.params.id.split("__");
 
-    if (!table) throw new Error("Нет такого листа или таблицы");
+        const table = {
+            tableId: ids[0],
+            sheetName: Buffer.from(ids[1], 'base64').toString('utf-8')
+        };
 
-    const data = await tools.getDataFromGoogleSheet(table.tableId, table.sheetName);
+        const data = await tools.getDataFromGoogleSheet(table.tableId, table.sheetName);
 
-    if (!data.state){
-      if(data.message == 'Ошибка при получение данных с таблицы: The caller does not have permission') throw new Error('Ошибка при получение данных с таблицы: \nПроблемы с доступом таблицы. Проверьте, что она действительно является публичной или ввели правильную ссылку!')
-      throw new Error(data.message)
+        if (!data.state) {
+            if (data.message === 'Ошибка при получение данных с таблицы: The caller does not have permission') {
+                throw new Error('Ошибка при получение данных с таблицы: \nПроблемы с доступом таблицы. Проверьте, что она действительно является публичной или ввели правильную ссылку!');
+            }
+            throw new Error(data.message);
+        }
+        
+        res.send({
+            state: true,
+            data: {
+                rows: data.data.rows,
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(200).json({ state: false, message: error.message });
     }
-    
-    res.send({ state: true, data: {
-      rows: data.data.rows,
-    } });
-  } catch (error) {
-    console.error(error);
-    res.status(200).json({ state: false, message: error.message });
-  }
 });
 
 app.post('/api/data/:id/append', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const newData = [Object.values(req.body.params)];
-    const table = await DataModel.findById(id);
+    try {
+        const ids = req.params.id.split("__");
 
-    if (!table) throw new Error("Нет такого листа или таблицы");
+        const table = {
+            tableId: ids[0],
+            sheetName: Buffer.from(ids[1], 'base64').toString('utf-8')
+        };
+        console.log(req.body)
+        const newData = [Object.values(req.body.params)];
 
-    const appendState = await tools.appendDataInGoogleSheet(table.tableId, table.sheetName, newData);
+        if (!table) throw new Error("Нет такого листа или таблицы");
 
-    if (!appendState.state){
-      if(appendState.message == 'Ошибка при получение данных с таблицы: The caller does not have permission') throw new Error('Ошибка при получение данных с таблицы: \nПроблемы с доступом таблицы. Проверьте, что она действительно является публичной или ввели правильную ссылку!')
-      throw new Error(appendState.message)
+        const appendState = await tools.appendDataInGoogleSheet(table.tableId, table.sheetName, newData);
+        console.log(newData)
+
+        if (!appendState.state) {
+            if (appendState.message === 'Ошибка при получение данных с таблицы: The caller does not have permission') {
+                throw new Error('Ошибка при получение данных с таблицы: \nПроблемы с доступом таблицы. Проверьте, что она действительно является публичной или ввели правильную ссылку!');
+            }
+            throw new Error(appendState.message);
+        }
+
+        res.json({ state: true });
+    } catch (error) {
+        res.status(200).json({ state: false, message: error.message });
     }
-
-    res.json({ state: true });
-  } catch (error) {
-    res.status(200).json({ state: false, message: error.message });
-  }
 });
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+// if ("SOCKET" in process.env) {
+//     const socket = process.env.SOCKET;
+//     if (fs.existsSync(socket)) {
+//         fs.unlinkSync(socket);
+//     }
+//     app.listen(socket, () => {
+//         fs.chmodSync(socket, 0o660);
+//         console.log(`Listening ${socket}`);
+//     });
+// } else if ("PORT" in process.env) {
+//     const port = process.env.PORT || 3000;
+//     app.listen(port, () => {
+//         console.log(`Listening on port ${port}/`);
+//     });
+// } else {
+//     const port = 3000;
+//     app.listen(port, () => {
+//         console.log(`Listening on port ${port}/`);
+//     });
+// }
+
 
 const server = app.listen(PORT, async () => {
   const { port } = server.address();
